134fa5f62a645c4a87bb3789c7c6f00d4f19ea84
theashitranjan <77455891+theashitranjan@users.noreply.github.com>
2025-03-31 10:53:39 +0530

    Update forgotPassword.php
    removed sensitive data

--- login-register/forgotPassword.php
+++ login-register/forgotPassword.php
@@ -33,8 +33,8 @@
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'krraaz776207@gmail.com';                     //SMTP username
-            $mail->Password   = 'Kunal@123';                               //SMTP password
+            $mail->Username   = ';                     //SMTP username
+            $mail->Password   = '';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
